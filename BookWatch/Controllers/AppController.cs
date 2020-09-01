cf1758fb9aacc359d0c889b5ffd2aa74773812b8
AsaVault <57707680+AsaVault@users.noreply.github.com>
2020-09-01 17:42:12 +0100

    saving Model bining section

---
+++ BookWatch/Controllers/AppController.cs
@@ -0,0 +1,37 @@
+﻿using BookWatch.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookWatch.Controllers
+{
+    public class AppController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewBag.Title = "About Us";
+            return View();
+        }
+
+        [HttpGet("contact")]
+        public IActionResult Contact()
+        {
+            //throw new InvalidOperationException("Something bad happens");
+            return View();
+        }
+
+        [HttpPost("contact")]
+        public IActionResult Contact(ContactViewModel model)
+        {
+            return View();
+        }
+    }
+}
--- BookWatch/Startup.cs
+++ BookWatch/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -16,14 +17,41 @@ namespace BookWatch
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+           // services.AddControllersWithViews();
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.UseDefaultFiles();
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                //Show Error Page
+                app.UseExceptionHandler("/error");
+            }
+            //app.UseDefaultFiles();
             app.UseStaticFiles();
             app.UseNodeModules();
+            
+            app.UseRouting();
+            app.UseEndpoints(cfg=> 
+            {
+                cfg.MapControllerRoute("Default",
+              "{controller}/{action}/{id?}",
+              new { controller = "App", Action = "Index" });
+            });
+
+            //What was in the video
+            //app.UseMvc(cfg =>
+            //{
+            //    cfg.MapRoute("Default",
+            //  "{controller}/{action}/{id?}",
+            //  new { controller = "App", Action = "Index" });
+            //});
         }
     }
 }
