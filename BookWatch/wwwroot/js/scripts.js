cc144db3612f43ad0f825a7b0bd1a3297e578fc2
AsaVault <57707680+AsaVault@users.noreply.github.com>
2020-09-01 11:02:18 +0100

    Jquery completed

---
+++ BookWatch/wwwroot/js/scripts.js
@@ -0,0 +1,21 @@
+﻿$(document).ready(function () {
+
+    var x = 0;
+    var s = "";
+
+    console.log("Hello Pluralsight");
+
+    var theForm = $("#theForm");
+    theForm.hide();
+
+    var button = $(".product-props li");
+    button.on("click", function () {
+        console.log("You have clicked on" + $(this).text());
+    });
+
+    var $loginToggle = $("#loginToggle");
+    var $popupForm = $(".popup-form");
+    $loginToggle.on("click", function () {
+        $popupForm.toggle(100);
+    });
+});
